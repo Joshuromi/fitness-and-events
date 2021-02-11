4d01734d419e7ee2552f0c5ba4f74361163a1c95
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-02-11 18:46:35 +0100

    installed and used bcrypt to hash user password

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,18 +1,30 @@
+const { response } = require("../app");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.models");
 const controller = {};
 
 controller.registerUser = async (req, res) => {
   const { firstName, lastName, email, password, dateCreated } = req.body;
-  const user = new User({
-    firstName,
-    lastName,
-    email,
-    password,
-    dateCreated,
-  });
+
   try {
-    const newUser = await user.save();
-    res.status(201).json(newUser);
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        dateCreated,
+      });
+      const newUser = await user.save();
+      res.status(201).json(newUser);
+    } else {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exist!" });
+    }
   } catch (error) {
     throw Error(`Error registering user: ${error}`);
   }
